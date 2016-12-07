30202f6bd340fe8b898a851af76e83a5c8dec96a
xiaolin3303 <xiaolin3303@163.com>
2016-12-07 17:53:32 +0800

    fix issue https://github.com/xiaolin3303/wx-charts/issues/2

--- dist/charts.js
+++ dist/charts.js
@@ -619,9 +619,32 @@ function Animation(opts) {
     opts.duration = typeof opts.duration === 'undefined' ? 1000 : opts.duration;
     opts.timing = opts.timing || 'linear';
 
-    var startTimeStamp = null;
+    var delay = 17;
 
+    var createAnimationFrame = function createAnimationFrame() {
+        if (typeof requestAnimationFrame !== 'undefined') {
+            return requestAnimationFrame;
+        } else if (typeof setTimeout !== 'undefined') {
+            return function (step, delay) {
+                setTimeout(function () {
+                    var timeStamp = +new Date();
+                    step(timeStamp);
+                }, delay);
+            };
+        } else {
+            return function (step) {
+                step(null);
+            };
+        }
+    };
+    var animationFrame = createAnimationFrame();
+    var startTimeStamp = null;
     function step(timestamp) {
+        if (timestamp === null) {
+            opts.onProcess && opts.onProcess(1);
+            opts.onAnimationFinish && opts.onAnimationFinish();
+            return;
+        }
         if (startTimeStamp === null) {
             startTimeStamp = timestamp;
         }
@@ -630,14 +653,14 @@ function Animation(opts) {
             var timingFunction = Timing[opts.timing];
             process = timingFunction(process);
             opts.onProcess && opts.onProcess(process);
-            requestAnimationFrame(step);
+            animationFrame(step, delay);
         } else {
             opts.onProcess && opts.onProcess(1);
             opts.onAnimationFinish && opts.onAnimationFinish();
         }
     }
 
-    requestAnimationFrame(step);
+    animationFrame(step, delay);
 }
 
 function drawCharts(type, opts, config, context) {
--- src/components/animation.js
+++ src/components/animation.js
@@ -4,9 +4,32 @@ export default function Animation (opts) {
     opts.duration = typeof opts.duration === 'undefined' ? 1000 : opts.duration;
     opts.timing = opts.timing || 'linear';
     
-    let startTimeStamp = null;
+    let delay = 17;
 
+    let createAnimationFrame = function () {
+        if (typeof requestAnimationFrame !== 'undefined') {
+            return requestAnimationFrame;
+        } else if (typeof setTimeout !== 'undefined') {
+            return function (step, delay) {
+                setTimeout(function () {
+                    let timeStamp = +new Date();
+                    step(timeStamp);
+                }, delay);
+            }
+        } else {
+            return function (step) {
+                step(null);
+            }
+        }
+    }
+    let animationFrame = createAnimationFrame();
+    let startTimeStamp = null;
     function step (timestamp) {
+        if (timestamp === null) {
+            opts.onProcess && opts.onProcess(1);
+            opts.onAnimationFinish && opts.onAnimationFinish();
+            return;
+        }
         if (startTimeStamp === null) {
             startTimeStamp = timestamp;
         } 
@@ -15,12 +38,12 @@ export default function Animation (opts) {
             let timingFunction = Timing[opts.timing];
             process = timingFunction(process);
             opts.onProcess && opts.onProcess(process);
-            requestAnimationFrame(step);
+            animationFrame(step, delay);
         } else {
             opts.onProcess && opts.onProcess(1);
             opts.onAnimationFinish && opts.onAnimationFinish();
         }
     }
 
-    requestAnimationFrame(step);
+    animationFrame(step, delay);
 }
