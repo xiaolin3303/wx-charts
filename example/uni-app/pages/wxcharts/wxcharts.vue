dd4f28b801c24388a153bc7f1383af14a4c5b804
16cheng <474119@qq.com>
2019-04-01 18:21:41 +0800

    uni-app示例

---
+++ example/uni-app/pages/wxcharts/wxcharts.vue
@@ -0,0 +1,357 @@
+<template>
+	<view class="qiun-columns">
+		<view class="qiun-padding" style="font-size: 32upx;">
+			<text>本插件使用wx-charts微信小程序图表工具【https://github.com/xiaolin3303/wx-charts】修改而来，感谢作者xiaolin3303。修改后的插件可以跨端使用，虽然没有Echarts及F2图表功能强大，但解决了H5端图表显示模糊等问题，并且调用简单方便、性能及体验极佳。</text>
+		</view>
+		<view class="qiun-padding">
+			<view class="qiun-tip" @tap="changeData()">修改柱状图数据</view>
+		</view>
+        <view class="qiun-bg-white qiun-title-bar" >
+        	<view class="qiun-title-dot-light">柱状图</view>
+        </view>
+        <view class="qiun-charts">
+        	<!--#ifdef H5-->
+        	<canvas canvasId="canvasColumn" class="charts" :style="{'width':cWidth*pixelRatio+'px','height':cHeight*pixelRatio+'px', 'transform': 'scale('+(1/pixelRatio)+')','margin-left':-cWidth*(pixelRatio-1)/2+'px','margin-top':-cHeight*(pixelRatio-1)/2+'px'}"></canvas>
+        	<!--#endif-->
+        	<!--#ifndef H5-->
+        	<canvas canvasId="canvasColumn" class="charts"></canvas>
+        	<!--#endif-->
+        </view>
+		<view class="qiun-bg-white qiun-title-bar qiun-common-mt" >
+			<view class="qiun-title-dot-light">折线图一</view>
+		</view>
+		<view class="qiun-charts">
+			<!--#ifdef H5-->
+			<canvas canvasId="canvasLineA" class="charts" :style="{'width':cWidth*pixelRatio+'px','height':cHeight*pixelRatio+'px', 'transform': 'scale('+(1/pixelRatio)+')','margin-left':-cWidth*(pixelRatio-1)/2+'px','margin-top':-cHeight*(pixelRatio-1)/2+'px'}" @touchstart="touchLineA"></canvas>
+			<!--#endif-->
+			<!--#ifndef H5-->
+			<canvas canvasId="canvasLineA" class="charts" @touchstart="touchLineA"></canvas>
+			<!--#endif-->
+		</view>
+		<view class="qiun-bg-white qiun-title-bar qiun-common-mt" >
+			<view class="qiun-title-dot-light">折线图二</view>
+		</view>
+		<view class="qiun-charts">
+			<!--#ifdef H5-->
+			<canvas canvasId="canvasLineB" class="charts" :style="{'width':cWidth*pixelRatio+'px','height':cHeight*pixelRatio+'px', 'transform': 'scale('+(1/pixelRatio)+')','margin-left':-cWidth*(pixelRatio-1)/2+'px','margin-top':-cHeight*(pixelRatio-1)/2+'px'}" @touchstart="touchLineB"></canvas>
+			<!--#endif-->
+			<!--#ifndef H5-->
+			<canvas canvasId="canvasLineB" class="charts" @touchstart="touchLineB"></canvas>
+			<!--#endif-->
+		</view>
+		<view class="qiun-bg-white qiun-title-bar qiun-common-mt" >
+			<view class="qiun-title-dot-light">区域图</view>
+		</view>
+		<view class="qiun-charts">
+			<!--#ifdef H5-->
+			<canvas canvasId="canvasArea" class="charts" :style="{'width':cWidth*pixelRatio+'px','height':cHeight*pixelRatio+'px', 'transform': 'scale('+(1/pixelRatio)+')','margin-left':-cWidth*(pixelRatio-1)/2+'px','margin-top':-cHeight*(pixelRatio-1)/2+'px'}" @touchstart="touchArea"></canvas>
+			<!--#endif-->
+			<!--#ifndef H5-->
+			<canvas canvasId="canvasArea" class="charts" @touchstart="touchArea"></canvas>
+			<!--#endif-->
+		</view>
+		<view class="qiun-bg-white qiun-title-bar qiun-common-mt" >
+			<view class="qiun-title-dot-light">饼状图</view>
+		</view>
+		<view class="qiun-charts">
+			<!--#ifdef H5-->
+			<canvas canvasId="canvasPie" class="charts" :style="{'width':cWidth*pixelRatio+'px','height':cHeight*pixelRatio+'px', 'transform': 'scale('+(1/pixelRatio)+')','margin-left':-cWidth*(pixelRatio-1)/2+'px','margin-top':-cHeight*(pixelRatio-1)/2+'px'}"></canvas>
+			<!--#endif-->
+			<!--#ifndef H5-->
+			<canvas canvasId="canvasPie" class="charts"></canvas>
+			<!--#endif-->
+		</view>
+		<view class="qiun-bg-white qiun-title-bar qiun-common-mt" >
+			<view class="qiun-title-dot-light">环形图</view>
+		</view>
+		<view class="qiun-charts">
+			<!--#ifdef H5-->
+			<canvas canvasId="canvasRing" class="charts" :style="{'width':cWidth*pixelRatio+'px','height':cHeight*pixelRatio+'px', 'transform': 'scale('+(1/pixelRatio)+')','margin-left':-cWidth*(pixelRatio-1)/2+'px','margin-top':-cHeight*(pixelRatio-1)/2+'px'}"></canvas>
+			<!--#endif-->
+			<!--#ifndef H5-->
+			<canvas canvasId="canvasRing" class="charts"></canvas>
+			<!--#endif-->
+		</view>
+		<view class="qiun-bg-white qiun-title-bar qiun-common-mt" >
+			<view class="qiun-title-dot-light">雷达图</view>
+		</view>
+		<view class="qiun-charts">
+			<!--#ifdef H5-->
+			<canvas canvasId="canvasRadar" class="charts" :style="{'width':cWidth*pixelRatio+'px','height':cHeight*pixelRatio+'px', 'transform': 'scale('+(1/pixelRatio)+')','margin-left':-cWidth*(pixelRatio-1)/2+'px','margin-top':-cHeight*(pixelRatio-1)/2+'px'}"></canvas>
+			<!--#endif-->
+			<!--#ifndef H5-->
+			<canvas canvasId="canvasRadar" class="charts"></canvas>
+			<!--#endif-->
+		</view>
+	</view>
+</template>
+
+<script>
+	import wxCharts from '../../components/wx-charts/wxcharts.js';
+	var _self;
+	var canvaColumn=null;
+	var canvaLineA=null;
+	var canvaLineB=null;
+	var canvaArea=null;
+	var Data={
+		Column:{categories:['2012', '2013', '2014', '2015', '2016', '2017'],series:[{name: '成交量1',data:[15, 20, 45, 37, 43, 34]},{name: '成交量2',data:[30, 40, 25, 14, 34, 18]}]},
+		ColumnB:{categories:['2013', '2014', '2015', '2016', '2017', '2018'],series:[{name: '新成交量3',data:[35, 36, 31, 33, 13, 34]},{name: '新成交量4',data:[18, 27, 21, 34, 14, 38]}]},
+		LineA:{categories:['2012', '2013', '2014', '2015', '2016', '2017'],series:[{name: '成交量A',data:[35, 20, 25, 37, 4, 20]},{name: '成交量B',data:[70, 40, 65, 100, 44, 68]},{name: '成交量C',data:[100, 80, 95, 150, 112, 132]}]},
+		LineB:{categories:['2012', '2013', '2014', '2015', '2016', '2017'],series:[{name: '成交量A',data:[35, 20, 25, 37, 4, 20]},{name: '成交量B',data:[70, 40, 65, 100, 44, 68]},{name: '成交量C',data:[100, 80, 95, 150, 112, 132]}]},
+		Area:{categories:['2012', '2013', '2014', '2015', '2016', '2017'],series:[{name: '成交量A',data:[35, 20, 25, 37, 4, 20]},{name: '成交量B',data:[70, 40, 65, 100, 44, 68]},{name: '成交量C',data:[100, 80, 95, 150, 112, 132]}]},
+		Pie:{series:[{ name: '一班', data: 50 }, { name: '二班', data: 30 }, { name: '三班', data: 20 }, { name: '四班', data: 18 }, { name: '五班', data: 8 }]},
+		Ring:{series:[{ name: '一班', data: 50 }, { name: '二班', data: 30 }, { name: '三班', data: 20 }, { name: '四班', data: 18 }, { name: '五班', data: 8 }]},
+		Radar:{categories: ['维度1', '维度2', '维度3', '维度4', '维度5', '维度6'],series:[{name: '成交量1',data: [90, 110, 165, 195, 187, 172]}, {name: '成交量2',data: [190, 210, 105, 35, 27, 102]}]}
+		}
+	
+	export default {
+		data() {
+			return {
+				cWidth:'',
+				cHeight:'',
+				pixelRatio:1
+			}
+		},
+		onLoad() {
+			_self = this;
+			//#ifdef H5
+			uni.getSystemInfo({
+				success: function (res) {
+					if(res.pixelRatio>1){
+						//正常这里给2就行，如果pixelRatio=3性能会降低一点
+						//_self.pixelRatio =res.pixelRatio;
+						_self.pixelRatio =2;
+					}
+				}
+			});
+			//#endif
+			this.cWidth=uni.upx2px(750);
+			this.cHeight=uni.upx2px(500);
+			
+		},
+		onReady() {
+			this.showColumn("canvasColumn",Data.Column);
+			this.showLineA("canvasLineA",Data.LineA);
+			this.showLineB("canvasLineB",Data.LineB);
+			this.showArea("canvasArea",Data.Area);
+			this.showPie("canvasPie",Data.Pie);
+			this.showRing("canvasRing",Data.Ring);
+			this.showRadar("canvasRadar",Data.Radar);
+		},
+		methods: {
+			showColumn(canvasId,chartData){
+				canvaColumn=new wxCharts({
+					canvasId: canvasId,
+					type: 'column',
+					legend:true,
+					fontSize:11,
+					background:'#FFFFFF',
+					pixelRatio:_self.pixelRatio,
+					animation: true,
+					categories: chartData.categories,
+					series: chartData.series,
+					xAxis: {
+						disableGrid:true
+					},
+					yAxis: {
+						//disabled:true
+					},
+					dataLabel: true,
+					width: _self.cWidth*_self.pixelRatio,
+					height: _self.cHeight*_self.pixelRatio,
+					extra: {
+						column: {
+						  width: _self.cWidth*_self.pixelRatio*0.45/chartData.categories.length
+						}
+					  }
+				});
+			},
+			showLineA(canvasId,chartData){
+				canvaLineA=new wxCharts({
+					canvasId: canvasId,
+					type: 'line',
+					fontSize:11,
+					legend:true,
+					background:'#FFFFFF',
+					pixelRatio:_self.pixelRatio,
+					categories: chartData.categories,
+					animation: true,
+					series: chartData.series,
+					xAxis: {
+						disableGrid:true,
+					},
+					yAxis: {
+						//disabled:true
+					},
+					width: _self.cWidth*_self.pixelRatio,
+					height: _self.cHeight*_self.pixelRatio,
+					dataLabel: true,
+					dataPointShape: true,
+					extra: {
+						lineStyle: 'straight'
+					},
+				});
+				
+			},
+			showLineB(canvasId,chartData){
+				canvaLineB=new wxCharts({
+					canvasId: canvasId,
+					type: 'line',
+					fontSize:11,
+					legend:true,
+					background:'#FFFFFF',
+					pixelRatio:_self.pixelRatio,
+					categories: chartData.categories,
+					animation: true,
+					series: chartData.series,
+					xAxis: {
+						disableGrid:true,
+					},
+					yAxis: {
+						//disabled:true
+					},
+					width: _self.cWidth*_self.pixelRatio,
+					height: _self.cHeight*_self.pixelRatio,
+					dataLabel: true,
+					dataPointShape: true,
+					extra: {
+						lineStyle: 'curve'
+					},
+				});
+			},
+			showArea(canvasId,chartData){
+				canvaArea=new wxCharts({
+					canvasId: canvasId,
+					type: 'area',
+					fontSize:11,
+					legend:true,
+					background:'#FFFFFF',
+					pixelRatio:_self.pixelRatio,
+					categories: chartData.categories,
+					animation: true,
+					series: chartData.series,
+					xAxis: {
+						disableGrid:true,
+					},
+					yAxis: {
+						//disabled:true
+					},
+					width: _self.cWidth*_self.pixelRatio,
+					height: _self.cHeight*_self.pixelRatio,
+					dataLabel: true,
+					dataPointShape: true,
+				});
+			},
+			showPie(canvasId,chartData){
+				new wxCharts({
+					canvasId: canvasId,
+					type: 'pie',
+					fontSize:11,
+					legend:true,
+					background:'#FFFFFF',
+					pixelRatio:_self.pixelRatio,
+					series: chartData.series,
+					animation: true,
+					width: _self.cWidth*_self.pixelRatio,
+					height: _self.cHeight*_self.pixelRatio,
+					dataLabel: true,
+				   });
+			},
+			showRing(canvasId,chartData){
+				new wxCharts({
+					canvasId: canvasId,
+					type: 'ring',
+					fontSize:11,
+					legend:true,
+					title: {
+						name: '70%',
+						color: '#7cb5ec',
+						fontSize: 25*_self.pixelRatio
+					},
+					subtitle: {
+						name: '收益率',
+						color: '#666666',
+						fontSize: 15*_self.pixelRatio
+					},
+					extra: {
+						ringWidth: 40*_self.pixelRatio,//圆环的宽度
+						pie: {
+						  offsetAngle: -45//圆环的角度
+						}
+					},
+					background:'#FFFFFF',
+					pixelRatio:_self.pixelRatio,
+					series: chartData.series,
+					animation: true,
+					width: _self.cWidth*_self.pixelRatio,
+					height: _self.cHeight*_self.pixelRatio,
+					disablePieStroke: true,
+					dataLabel: true,
+				});
+				
+			},
+			showRadar(canvasId,chartData){
+				new wxCharts({
+					canvasId: canvasId,
+					type: 'radar',
+					fontSize:11,
+					legend:true,
+					background:'#FFFFFF',
+					pixelRatio:_self.pixelRatio,
+					animation: true,
+					categories: chartData.categories,
+					series: chartData.series,
+					width: _self.cWidth*_self.pixelRatio,
+					height: _self.cHeight*_self.pixelRatio,
+					extra: {
+						radar: {
+							max: 200//雷达数值的最大值
+						}
+					}
+				});
+			},
+			changeData(){
+				//这里只做了柱状图数据动态更新，其他图表同理。
+				canvaColumn.updateData({
+					series: Data.ColumnB.series,
+					categories: Data.ColumnB.categories
+				});
+			},
+			touchLineA(e){
+				canvaLineA.showToolTip(e, {
+					format: function (item, category) {
+						return category + ' ' + item.name + ':' + item.data 
+					}
+				});
+			},
+			touchLineB(e){
+				canvaLineB.showToolTip(e, {
+					format: function (item, category) {
+						return category + ' ' + item.name + ':' + item.data 
+					}
+				});
+			},
+			touchArea(e){
+				canvaArea.showToolTip(e, {
+					format: function (item, category) {
+						return category + ' ' + item.name + ':' + item.data 
+					}
+				});
+			},
+		}
+	}
+</script>
+
+<style>
+page{background:#F2F2F2;}
+.qiun-padding{padding:2%; width:96%;}
+.qiun-wrap{display:flex; flex-wrap:wrap;}
+.qiun-columns{display:flex; flex-direction:column !important;}
+.qiun-common-mt{margin-top:10upx;}
+.qiun-bg-white{background:#FFFFFF;}
+.qiun-title-bar{width:96%; padding:10upx 2%; flex-wrap:nowrap;}
+.qiun-title-dot-light{border-left: 10upx solid #0ea391; padding-left: 10upx; font-size: 32upx;color: #000000}
+.qiun-charts{width: 750upx; height:500upx;background-color: #FFFFFF;}
+.charts{width: 750upx; height:500upx;background-color: #FFFFFF;}
+.qiun-tip {display:block; width:auto; overflow:hidden; padding:15upx; height:30upx; line-height:30upx; margin:10upx; background:#ff9933; font-size:30upx; border-radius:8upx;justify-content:center; text-align:center;border: 1px solid #dc7004;color: #FFFFFF;}
+</style>
